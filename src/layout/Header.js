8bfddbf3992cf68b80023ca673f8deca6492a995
Mateus Gomes <mateuscostagomes000@gmail.com>
2024-01-19 08:39:21 -0300

    feat: Header finished

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,21 +1,31 @@
 import { Link } from "react-router-dom"
-import Home from "../pages/Home"
 import Logo from '../img/costs_logo.png'
-import Company from "../pages/Company"
-import NewProjects from "../pages/NewProjects"
-import Contact from "../pages/Contact"
+import Container from './Container'
+import styles from "./Header.module.css"
 
 function Header(){
     return(
-        <div>
-            <Link to={Home}>
-                <img src={Logo} alt="Logo" />
-            </Link>
-            <Link to='/'>Home</Link>
-            <Link to='/Company'>Empresa</Link>
-            <Link to='/NewProjects'>Projetos</Link>
-            <Link to='Contact'>Contatos</Link>
-        </div>
+        <nav className={styles.navbar}>
+            <Container>
+                <Link to='/'>
+                    <img src={Logo} alt="Logo" />
+                </Link>
+                <ul className={styles.list}>
+                    <li className={styles.item}>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link to='/Company'>Empresa</Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link to='/NewProjects'>Projetos</Link>
+                    </li>
+                    <li className={styles.item}>
+                        <Link to='/Contact'>Contatos</Link>
+                    </li>
+                </ul>
+            </Container>
+        </nav>
     )
 }
 
