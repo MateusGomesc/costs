46b18d7c4b47ac0ea9c2ca20c5fd1568d2d73dac
Mateus Gomes <mateuscostagomes000@gmail.com>
2024-01-24 14:21:14 -0300

    feat: request select categories from backend

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -5,7 +5,10 @@ function Select({ text, name, options, handleOnChange, value }){
         <div className={styles.select}>
             <label htmlFor={name}>{text}:</label>
             <select name={name} id={name}>
-                <option>Selecione uma opção</option>
+                <option selected disabled>Selecione uma opção</option>
+                {options.map((option) => (
+                    <option value={option.id} key={option.id}>{option.name}</option>
+                ))}
             </select>
         </div>
     )
--- src/components/projects/ProjectsForm.js
+++ src/components/projects/ProjectsForm.js
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectsForm.module.css'
 
 function ProjectsForm({ btnText }){
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => setCategories(data))
+            .catch(error => console.log(error))
+    }, [])
+
     return(
         <form className={styles.form}>
             <Input 
@@ -21,7 +36,7 @@ function ProjectsForm({ btnText }){
             <Select 
                 name='categoryID'
                 text='Selecione a categoria'
-
+                options={categories}
             />
             <SubmitButton 
                 text={btnText}
